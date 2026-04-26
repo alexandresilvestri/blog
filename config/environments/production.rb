5387aa83a5751803b6e99a0cfea1470b31f4376b
Alexandre Silvestri <airtontunni@gmail.com>
2026-04-25 22:02:19 -0300

    first commit

---
+++ config/environments/production.rb
@@ -0,0 +1,67 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  config.enable_reloading = false
+
+  config.eager_load = true
+
+  config.consider_all_requests_local = false
+
+  config.action_controller.perform_caching = true
+
+  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.asset_host = "http://assets.example.com"
+
+  config.active_storage.service = :local
+
+  config.assume_ssl = true
+
+  config.force_ssl = true
+
+  # Skip http-to-https redirect for the default health check endpoint.
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+
+  config.log_tags = [ :request_id ]
+  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+
+  config.silence_healthcheck_path = "/up"
+
+  config.active_support.report_deprecations = false
+
+  config.cache_store = :solid_cache_store
+
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: "alexandregaspar.online" }
+
+  # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
+  # config.action_mailer.smtp_settings = {
+  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
+  #   password: Rails.application.credentials.dig(:smtp, :password),
+  #   address: "smtp.example.com",
+  #   port: 587,
+  #   authentication: :plain
+  # }
+
+  config.i18n.fallbacks = true
+
+  config.active_record.dump_schema_after_migration = false
+
+  config.active_record.attributes_for_inspect = [ :id ]
+
+  config.hosts = [
+     "alexandregaspar.online",
+     /.*\.alexandregaspar\.online/
+   ]
+
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+end
