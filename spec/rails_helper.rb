ef5bb48c9dcdbc57166e82bf337d83be403aa8c6
Alexandre Silvestri <airtontunni@gmail.com>
2026-06-12 22:08:46 -0300

    finish testcotnainers config + simple tests for posts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,17 +23,10 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
-# Ensures that the test database schema matches the current schema file.
-# If there are pending migrations it will invoke `db:test:prepare` to
-# recreate the test database by loading the schema.
-# If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
-end
+# Schema is loaded against the Testcontainers Postgres in
+# spec/support/test_containers.rb (before(:suite)).
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
--- spec/support/test_containers.rb
+++ spec/support/test_containers.rb
@@ -1,17 +1,23 @@
-# frozen_string_literal = true
+# frozen_string_literal: true
 
-module  Testcontainers
+require "testcontainers/postgres"
+
+module Testcontainers
   class << self
     attr_accessor :postgres
   end
 end
 
-Rspec.configure do |config|
+RSpec.configure do |config|
   config.before(:suite) do
-    Testcontainers::PostgresContainer.new("postgres:16-alpine")
-    Testcontainers::PostgresContainer.start
+    Testcontainers.postgres = Testcontainers::PostgresContainer.new("postgres:16-alpine").start
 
-    ActiveRecord::Base.establish_connection(TestContainers.postgres.database_url)
-    ActiveRecord::MigrationContext.new(Rails.root.join("db/migrate")).migrate
+    ActiveRecord::Base.establish_connection(Testcontainers.postgres.database_url)
+    load Rails.root.join("db/schema.rb")
+  end
+
+  config.after(:suite) do
+    Testcontainers.postgres&.stop
+    Testcontainers.postgres&.remove
   end
 end
