b6eef03218a016eebad0061b756a90d98d06809d
Alexandre Silvestri <airtontunni@gmail.com>
2026-06-14 21:41:20 -0300

    turn seeds only for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-Post.create!(title: "First Post", post: "First post seed! Very nice.")
-Post.create!(title: "Second Post", post: "Second post seed! I'm the best huahua")
+if Rails.env.development?
+  Post.create!(title: "First Post", post: "First post seed! Very nice.")
+  Post.create!(title: "Second Post", post: "Second post seed! I'm the best huahua")
+end
