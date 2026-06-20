31509b4030deb9d9b5b69e535bb812dd0758b42c
Alexandre Silvestri <airtontunni@gmail.com>
2026-06-20 09:52:39 -0300

    dev seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 if Rails.env.development?
-  Post.create!(title: "First Post", post: "First post seed! Very nice.")
-  Post.create!(title: "Second Post", post: "Second post seed! I'm the best huahua")
+  Post.create!(title: 'First Post', post: 'First post seed! Very nice.')
+  Post.create!(title: 'Second Post', post: "Second post seed! I'm the best huahua")
 end
