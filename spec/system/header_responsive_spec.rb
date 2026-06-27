9d6fa7f68ea857627c460d60198621975eca4246
Alexandre Silvestri <airtontunni@gmail.com>
2026-06-27 16:00:37 -0300

    modify header design

--- spec/system/header_responsive_spec.rb
+++ spec/system/header_responsive_spec.rb
@@ -35,4 +35,48 @@ RSpec.describe 'Header responsiveness', type: :system do
       expect(width).to eq(height)
     end
   end
+
+  it 'sizes the github and gitlab icons w-6 on mobile and w-8 on desktop' do
+    visible_width = lambda do |alt|
+      page.evaluate_script(
+        "Array.from(document.querySelectorAll(\"img[alt='#{alt}']\"))" \
+        '.map(e => e.offsetWidth).find(v => v > 0) || 0'
+      )
+    end
+
+    current_window.resize_to(375, 700)
+    visit root_path
+    %w[Github\ Profile Gitlab\ Profile].each do |alt|
+      expect(visible_width.call(alt)).to eq(24)
+    end
+
+    current_window.resize_to(1280, 800)
+    visit root_path
+    %w[Github\ Profile Gitlab\ Profile].each do |alt|
+      expect(visible_width.call(alt)).to eq(32)
+    end
+  end
+
+  it 'nudges github and gitlab below the fox on mobile, aligned on desktop' do
+    visible_top = lambda do |alt|
+      page.evaluate_script(
+        "Array.from(document.querySelectorAll(\"img[alt='#{alt}']\"))" \
+        '.map(e => e.getBoundingClientRect().top).find(v => v > 0) || 0'
+      )
+    end
+
+    current_window.resize_to(375, 700)
+    visit root_path
+    fox_top = visible_top.call('Fox')
+    %w[Github\ Profile Gitlab\ Profile].each do |alt|
+      expect(visible_top.call(alt)).to be > fox_top
+    end
+
+    current_window.resize_to(1280, 800)
+    visit root_path
+    fox_top = visible_top.call('Fox')
+    %w[Github\ Profile Gitlab\ Profile].each do |alt|
+      expect(visible_top.call(alt)).to eq(fox_top)
+    end
+  end
 end
